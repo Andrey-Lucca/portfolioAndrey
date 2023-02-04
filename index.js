c24dbc348ef326b22da742d1a2a29c67605c2863
Andrey Lucca <122308400+Andrey-Lucca@users.noreply.github.com>
2023-02-04 15:58:13 -0300

    Uptade link

--- index.js
+++ index.js
@@ -6,5 +6,3 @@ function showEmail () {
 function downloadCv() {
   window.location.href = 'https://drive.google.com/u/0/uc?id=1I-4tzwwq8D2F-RbUAJmgbA2OOLy-YCbn&export=download'
 }
-
-alert(`obs: a responsividade está demorando a carregar, mas logo será resolvido`)
