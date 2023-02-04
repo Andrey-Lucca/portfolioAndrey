7e0522917ac3e16f8dd48132b36a6a39e2adb650
Andrey Lucca <122308400+Andrey-Lucca@users.noreply.github.com>
2023-02-04 15:18:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function showEmail () {
+  alert("EMAIL: drey_lucca@hotmail.com")
+}
+
+function downloadCv() {
+  window.location.href = 'https://drive.google.com/u/0/uc?id=1I-4tzwwq8D2F-RbUAJmgbA2OOLy-YCbn&export=download'
+}
+
+alert(`A responsividade está demorando para ser lida, portanto, caso dê erro, atualize a página`)
